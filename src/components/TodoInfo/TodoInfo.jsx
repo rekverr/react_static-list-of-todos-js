5f004ff6a5f68204415b741b5270589bf50eaed2
Viktor Shchehelskyi <victorshegelsky@gmail.com>
2025-04-11 17:54:53 +0300

    redid

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,7 +4,6 @@ import './TodoInfo.scss';
 export const TodoInfo = ({ todo }) => (
   <article
     className={`TodoInfo ${todo.completed === true && 'TodoInfo--completed'}`}
-    key={todo.user.id}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
